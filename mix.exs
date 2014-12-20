3eadabe05bddbffe9e3d5cf17a9384319d8756a1
Michael Truog <mjtruog@gmail.com>
2014-12-20 13:59:16 -0800

    Version 1.4.0.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Quickrand.Mixfile do
 
   def project do
     [app: :quickrand,
-     version: "1.4.0-rc.4",
+     version: "1.4.0",
      language: :erlang,
      description: description,
      package: package,
