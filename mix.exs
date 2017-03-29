cae86199e2206f1fa5faaa3f0d928841eb2ee50c
Michael Truog <mjtruog@gmail.com>
2017-03-28 18:25:16 -0700

    Version 1.7.0.

--- mix.exs
+++ mix.exs
@@ -1,9 +1,12 @@
+#-*-Mode:elixir;coding:utf-8;tab-width:2;c-basic-offset:2;indent-tabs-mode:()-*-
+# ex: set ft=elixir fenc=utf-8 sts=2 ts=2 sw=2 et nomod:
+
 defmodule Quickrand.Mixfile do
   use Mix.Project
 
   def project do
     [app: :quickrand,
-     version: "1.6.0",
+     version: "1.7.0",
      language: :erlang,
      description: description(),
      package: package(),
